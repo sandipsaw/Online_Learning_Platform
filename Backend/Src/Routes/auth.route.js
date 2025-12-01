8ed18fa62d6eef9b58a196f7cb2d14cf2cd16054
sandip saw <sandipsaw449@gmail.com>
2025-12-02 00:27:37 +0530

    register,login,and get user

--- Backend/Src/Routes/auth.route.js
+++ Backend/Src/Routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const validator = require('../Middleware/validator.middleware')
-const authController = require('../Controller/auth.controller')
+const authController = require('../Controller/auth.controller');
+const authMiddleware = require('../Middleware/auth.middleware');
 const router = express.Router();
 
 router.post('/register',validator.registerUserValidation,authController.registerUser)
-
+router.get('/login',validator.loginUserValidation,authController.loginUser)
+router.get('/me',authMiddleware.authMiddlewares,authController.getUser)
 module.exports = router
